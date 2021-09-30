ec49d93ce0884566af6308568acce6ca144d5ecb
VikingDDM <chrisdulin4ever@gmail.com>
2021-09-30 03:27:03 -0300

    Add on:ready method (called after editor:ready event)

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,13 @@
+import Vue from 'vue';
 import EmailEditor from './EmailEditor.vue';
 
 const Components = {
   EmailEditor,
 };
+
+Object.keys(Components).forEach((name) => {
+  Vue.component(name, Components[name]);
+});
 
 export { EmailEditor };
 export default Components;
